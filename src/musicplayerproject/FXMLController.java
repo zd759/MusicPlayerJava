4c32d022de4d1628a7aa35f34c28b00780d13d71
Zara Duncanson <zara.duncanson@gmail.com>
2020-05-31 20:35:42 +0800

    Initial commit

---
+++ src/musicplayerproject/FXMLController.java
@@ -0,0 +1,25 @@
+/*
+ * FXMLController class
+ */
+package musicplayerproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Zara
+ */
+public class FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/musicplayerproject/MusicPlayerProject.java
@@ -0,0 +1,30 @@
+/* Name: Zara Duncanson
+ * Student ID: P229768
+ * Date: 27/05/20
+ * Task: Final Project
+ * This program lets a user login using hashing techniques on their password.
+ * Once logged in, users are able to chat and also use a media player which 
+ * can load audio files and save to .csv file using a third party library. Songs 
+ * are sorted automatically upon upload and can then be searched. Tracks are 
+ * stored in a dynamic data-structure (doubly-linked list) and users are stored 
+ * as objects with attributes in a list. It also implements a custom GUI using a 
+ * FXML document created using Scene Builder. The FXML is controlled by the 
+ * FXMLController to execute methods and functions. Includes help files accessed 
+ * via the menu bar item. Uses Java FX and 'Open CSV' third party library.
+ */
+package musicplayerproject;
+
+/**
+ *
+ * @author Zara
+ */
+public class MusicPlayerProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
